296c635162ba0dabfdbff9d77089ca90d3abe2f1
Hershy Loeb <yhloeb@gmail.com>
2018-01-15 06:17:27 +0000

    Added contact form mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,9 @@
+class ContactMailer < ActionController::Base
+  default to: 'test@yhloeb.com'
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
+  end
+end
