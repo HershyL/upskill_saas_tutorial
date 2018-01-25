441077cda568f77c999fec81044e1e9873dc431b
Hershy Loeb <yhloeb@gmail.com>
2018-01-25 00:23:40 +0000

    Added code comments for Stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   
   before_action :configure_premitted_parameters, if: :devise_controller?
   
+  # Whitelist the following form fields so that we can process them, if coming from a Devise sign up form.
   protected
     def configure_premitted_parameters
       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:stripe_card_token, :email, :password, :password_confirmation) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
   belongs_to :plan
   
   attr_accessor :stripe_card_token
+  # If pro users passes validations (email, password, etc),
+  # then call Stripe and tell Stripe to set up a subscription
+  # upon charging the customers card.
+  # Stripe respondes with customer data.
+  # Store customer.id as customer token and save the user.
   def save_with_subscription
     if valid?
       customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
