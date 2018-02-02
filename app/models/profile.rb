45b1b45083983d89b6f0ca7d80dcdd8765435232
Hershy Loeb <yhloeb@gmail.com>
2018-02-02 18:26:08 +0000

    Added favicon and blank avatar

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,6 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   has_attached_file :avatar, 
                     :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
-                    :default_url => "/images/:style/missing.png"
+                    :default_url => "https://www.activia.co.uk/blog/wordpress/wp-content/uploads/2016/11/blank-avatar.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
